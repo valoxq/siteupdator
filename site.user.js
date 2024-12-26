aab16b71b34ea7047fb6a8b8c124ece3ff288f53
valoxq <155094318+valoxq@users.noreply.github.com>
2024-12-26 10:38:10 +0200

    update

--- site.user.js
+++ site.user.js
@@ -16,7 +16,7 @@
 // ==/UserScript==
 
 (function() {
-    // Get host and url then check continue or not//
+    // Get host and url then check continue or not//Check
     var currentHost = window.location.host;
     var currentUrl = window.location.href;
     if (
